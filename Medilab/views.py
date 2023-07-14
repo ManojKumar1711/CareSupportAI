9ef28e6cd115c15e20109319187c2e64e4372a39
her_username <ragh20ad039@rmkcet.ac.in>
2023-07-14 14:08:39 +0530

    minor bug fixes

--- Medilab/views.py
+++ Medilab/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
 # Create your views here.
 
 def home(request):
@@ -19,3 +19,24 @@ def chat(request):
 
 def contact(request):
     return render(request, "Medilab/contact.html")
+
+def login(request):
+    return render(request,"Medilab/login.html")
+
+def registration(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        mobile= request.POST['mobile']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name,last_name=last_name)
+        user.save()
+        print("user created")
+        return redirect('/')
+    else:
+        return render(request,"Medilab/registration.html")
+
